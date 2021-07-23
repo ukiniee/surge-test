49e32ee04bd910ca9d77c4614e3751c99cdf6769
ukiniee <62276906+ukiniee@users.noreply.github.com>
2021-07-23 12:12:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          You can start your app here
+        </p>
+        <p>
+          Or get hypnotized by this nice circly thingy
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
